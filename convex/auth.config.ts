170571cd62e864b5eb7d3d16ef885326feafcce3
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2024-03-26 09:30:45 +0100

    initial project setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+export default {
+  providers: [
+    {
+      // Issuer inside the JWT Templates of the clerk project
+      domain: "https://helped-oryx-84.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+}
